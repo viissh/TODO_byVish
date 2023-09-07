0273ee288fd1b8937e7fe7cde6f455433e5d8687
Vishal <vish8062@example.com>
2023-09-07 18:11:39 +0530

    added authentication

--- app.py
+++ app.py
@@ -1,33 +1,115 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 
 app = Flask(__name__)
 app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Users.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todoApp.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = "abc"
+
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# db.init_app(app)python
+
+
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True,
+                         nullable=False)
+    password = db.Column(db.String(250),
+                         nullable=False)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(200), nullable = False)
     title = db.Column(db.String(200), nullable = False)
     desc = db.Column(db.String(500), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f"{self.sno} - {self.title}" 
+        return f"{self.sno} - {self.title}"
 
 
-@app.route('/',methods=['GET','POST'])
-def create():
+# Creates a user loader callback that returns the user object given an id
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
+ 
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+  # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        # Add the user to the database
+        db.session.add(user)
+        # Commit the changes made
+        db.session.commit()
+        # Once user account created, redirect them
+        # to login route (created later on)
+        return redirect(url_for("login"))
+    # Renders sign_up template if user made a GET request
+    return render_template("sign_up.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # If a post request was made, find the user by
+    # filtering for the username
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        # Check if the password entered is the
+        # same as the user's password
+        if user is None:
+            return render_template("login.html")
+            
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("home"))
+            
+        # Redirect the user back to the home
+        # (we'll create the home route in a moment)
+    return render_template("login.html")
+
+
+
+@app.route("/")
+def home():
+    # Render home.html on "/" route
+    return render_template("home.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+    
+
+
+
+@app.route('/todos/<string:name>',methods=['GET','POST'])
+def create(name):
+    
     if request.method == 'POST':
+        userName = name
         title = request.form['title']
         desc = request.form['desc']
 
-        todo = Todo(title = title, desc = desc)
+        todo = Todo(username = userName, title = title, desc = desc)
         db.session.add(todo)
         db.session.commit()
-    allTodo = Todo.query.all()
+    #allTodo = Todo.query.all()
+    allTodo = Todo.query.filter_by(username = name)  #.first()
+    print(allTodo)
     return render_template('index.html',allTodo =allTodo)
 
 @app.route('/update/<int:Sno>', methods=['GET','POST'])
@@ -40,7 +122,7 @@ def update(Sno):
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
-        return redirect('/')
+        return redirect('/todos')
 
     todo = Todo.query.filter_by(sno = Sno).first()
     return render_template('update.html',todo =todo) 
@@ -50,7 +132,7 @@ def delete(Sno):
     todo = Todo.query.filter_by(sno = Sno).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect('/')
+    return redirect('/todos')
 
 @app.route('/products')
 def products():
